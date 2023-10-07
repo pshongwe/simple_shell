9881f0308e902134c9f51868652a01579c6ddc16
Pibo Shongwe <piboman@yahoo.com>
2023-10-07 07:33:23 -0700

    update

--- _string.c
+++ _string.c
@@ -8,24 +8,16 @@
  */
 int _strcmp(const char *s1, const char *s2)
 {
-	if (!s1 && !s2)
-	{
-		return (0);
-	}
-	else if (!s1)
-	{
-		return (-1);
-	}
-	else if (!s2)
-	{
-		return (1);
-	}
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 != *s2)
+{
+return (*s1 - *s2);
+}
+s1++;
+s2++;
+}
+return (*s1 - *s2);
 }
 
 /**
