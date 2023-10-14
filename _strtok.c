07011b6dd58d2eb80b0aec54aff84aa35b1afbbc
Pibo Shongwe <piboman@yahoo.com>
2023-10-14 23:13:27 +0200

    update

--- _strtok.c
+++ _strtok.c
@@ -67,3 +67,36 @@ if (i == 0)
 return (NULL);
 }
 }
+
+/**
+ * _space - removes spaces
+ * @str: string to remove spaces
+ * Return: return clean string
+ */
+char *_space(char *str)
+{
+	int x = 0, y = 0;
+	char *b;
+
+	b = malloc(sizeof(char) * (_strlen(str) + 1));
+	if (b == NULL)
+	{
+		free(b);
+		return (NULL);
+	}
+	while (str[x] == ' ')
+		x++;
+	while (str[x + 1] != '\0')
+	{
+		b[y] = str[x];
+		x++;
+        y++;
+	}
+	b[y] = '\0';
+	if (b[0] == '\0' || b[0] == '#')
+	{
+		free(b);
+		return ("\0");
+	}
+	return (b);
+}
